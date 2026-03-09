7bf0d76f07c60c127d8ed29d71e47bcbf1b19c4c
ahaanthakur12 <ahaanthakur12@gmail.com>
2026-03-09 11:39:48 +0530

    Added Supabase DB, run history.

--- dummy.py
+++
@@ -1,5 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-import os
-print('SUPABASE_URL:', os.getenv('SUPABASE_URL'))
-print('SUPABASE_KEY:', os.getenv('SUPABASE_KEY')[:20] if os.getenv('SUPABASE_KEY') else 'NOT FOUND')
